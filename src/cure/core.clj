ba6fa1e37fe006786e729613cd12014616a25dcf
Jules White <jules.white@gmail.com>
2017-02-07 20:48:51 -0600

    - Addition of test cases - Converted use to require

--- src/cure/core.clj
+++ src/cure/core.clj
@@ -1,8 +1,11 @@
 (ns cure.core
-  (:gen-class))
+ ;(:gen-class)
+)
 
-(use 'loco.core 'loco.constraints)
+(require '[loco.core :as solver] '[loco.constraints :as con])
 
+;; Need some type of command line interface to pass in configuration
+;; definitions in Clojure
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
@@ -12,9 +15,10 @@
 ;; Creates a boolean variable indicating the presence or
 ;; absence of a feature in a configuration
 (defn feature [name]
-  ($in name 0 1))
+  (con/$in name 0 1))
 
-;; Begining of an attempt to use the built-in knapsack cosntraint
+;; Uses the built-in knapsack constraint to define a resource
+;; constraint governing the slection of features
 (defn resource_limit [rname amount consumers]
   (let [ consumer_pairs (seq consumers)
          occurence_vars (map #(first %) consumer_pairs)
@@ -22,27 +26,32 @@
          value_vars (take (count consumers) (repeat 1))
          weight_vars (map #(second %) consumer_pairs)]
     (list
-      ($in rname 0 amount)
-      ($in vname 0 (count consumer_pairs))
-      ($knapsack weight_vars value_vars occurence_vars rname vname)
-      ($<= rname amount))))
+      (con/$in rname 0 amount)
+      (con/$in vname 0 (count consumer_pairs))
+      (con/$knapsack weight_vars value_vars occurence_vars rname vname)
+      (con/$<= rname amount))))
 
 ;; Specifies that a feature requires between min..max of the target features or exactly count of them
 (defn requires
-  ([parent min max children] ($if ($= parent 1) ($and ($<= (apply $+ children) max) ($>= (apply $+ children) min))))
-  ([parent count children] ($if ($= parent 1) ($= (apply $+ children) max))))
+  ([parent min max children]
+     (con/$if (con/$= parent 1)
+              (con/$and
+                (con/$<= (apply con/$+ children) max)
+                (con/$>= (apply con/$+ children) min))))
+  ([parent count children]
+     (con/$if (con/$= parent 1) (con/$= (apply con/$+ children) max))))
 
 ;; Specifies that the provided parent is mutually exclusive with all features specified in children
 (defn excludes [parent children]
-  ($if ($= parent 1) ($= (apply $+ children) 0)))
+  (con/$if (con/$= parent 1) (con/$= (apply con/$+ children) 0)))
 
 ;; Forces the selection of the given features
 (defn selected [features]
-  ($= (apply $+ features) (count features)))
+  (con/$= (apply con/$+ features) (count features)))
 
 ;; Prevents the selection of the given features
 (defn deselected [features]
-  ($= (apply $+ features) 0))
+  (con/$= (apply con/$+ features) 0))
 
 ;; Returns true if all of the specified features are in the
 ;; configuration (E.g., solution) previously returned
@@ -63,7 +72,7 @@
 ;; :timeout millis -- maximum time to look for a solution (see: https://github.com/aengelberg/loco)
 ;;
 (defn all-configurations [model & options]
-  (apply solutions (args model options)))
+  (apply solver/solutions (args model options)))
 
 ;; Returns a singlve valid configuration
 ;;
@@ -76,7 +85,7 @@
 ;; :timeout millis -- maximum time to look for a solution (see: https://github.com/aengelberg/loco)
 ;;
 (defn configuration [model & options]
-  (apply solution (args model options)))
+  (apply solver/solution (args model options)))
 
 ;; Constructs a feature model with the given constraints
 (defn feature-model [constraints]
@@ -107,7 +116,7 @@
 ;;
 ;; Select as many of a, b, d as possible:
 ;;
-;; (configuration tm :maximize ($+ :a :b :d))
+;; (configuration tm :maximize (con/$+ :a :b :d))
 ;;
 ;; Get all configurations
 ;;
--- test/cure/core_test.clj
+++ test/cure/core_test.clj
@@ -1,11 +1,84 @@
 (ns cure.core-test
   (:require [clojure.test :refer :all]
-            [cure.core :refer :all]))
+            [cure.core :refer :all])
+  )
+
+(deftest unbounded-constraints-test
+  (testing "a feature model with no constraints or goals"
+    (let [ model (feature-model [
+                    (feature :a)
+                    (feature :b)
+                    (feature :c)
+                    (feature :d)
+                    (feature :e)])
+           solutions (all-configurations model)]
+      (is (= 32 (count solutions))))))
 
 
+(deftest pure-logical-constraints-test
+  (testing "a feature model with only logical constraints"
+    (let [ model (feature-model [
+                    (feature :a)
+                    (feature :b)
+                    (feature :c)
+                    (feature :d)
+                    (feature :e)
+                    (requires :a 2 3 [:b :c :d])
+                    (requires :e 1 1 [:b :d])
+                    (excludes :e [:c])
+                    (requires :b 1 1 [:e])
+                    (excludes :b [:d])
+                    (requires :c 1 1 [:d])
+                    (selected [:a :c])])
+           solution (configuration model)]
+      (is (has solution :a))
+      (is (has solution :c))
+      (is (has solution :d))
+      (is (not (has solution :e))))))
 
-(deftest a-test
-  (testing "FIXME, I fail."
+
+(deftest pure-logical-constraints-contradictory
+  (testing "a feature model with only logical constraints that contradict each other"
+    (let [ model (feature-model [
+                    (feature :a)
+                    (feature :b)
+                    (feature :c)
+                    (feature :d)
+                    (feature :e)
+                    (requires :a 2 3 [:b :c :d])
+                    (requires :e 1 1 [:b :d])
+                    (excludes :e [:c])
+                    (requires :b 1 1 [:e])
+                    (excludes :b [:d])
+                    (excludes :d [:c])
+                    (requires :c 1 1 [:d])
+                    (selected [:a :c])])
+           solutions (all-configurations model)]
+      (is (= (count solutions) 0)))))
+
+
+(deftest pure-logical-constraints-contradictory-goals
+  (testing "a feature model with only logical constraints and goals (e.g. selected/deslected) that contradict each other"
+    (let [ model (feature-model [
+                    (feature :a)
+                    (feature :b)
+                    (feature :c)
+                    (feature :d)
+                    (feature :e)
+                    (requires :a 2 3 [:b :c :d])
+                    (requires :e 1 1 [:b :d])
+                    (excludes :e [:c])
+                    (requires :b 1 1 [:e])
+                    (excludes :b [:d])
+                    (requires :c 1 1 [:d])
+                    (selected [:a :c])
+                    (deselected [:a :c])])
+           solutions (all-configurations model)]
+      (is (= (count solutions) 0)))))
+
+
+(deftest mixed-logical-and-resource-constraints-test
+  (testing "a mixed feature model with a single resource constraint"
     (let [ model (feature-model [
                     (feature :a)
                     (feature :b)
@@ -19,4 +92,80 @@
                     (selected [:a])])
            solution (configuration model)]
       (is (has solution :a))
-      (is (>= (+ (get solution :b) (get solution :c) (get solution :d)) 2)))))
+      (is (or (has solution :b) (has solution :c)))
+      (is (or (has solution :c) (has solution :d)))
+      (is (>= (+ (get solution :b) (get solution :c) (get solution :d)) 2))
+      (is (not (and (has solution :b) (has solution :d))))
+      (is (<= (get solution :cpu) 18)))))
+
+
+(deftest pure-resource-constraints-test-viable
+  (testing "a feature model with only resource constraints and a viable solution"
+    (let [ model (feature-model [
+                    (feature :a)
+                    (feature :b)
+                    (feature :c)
+                    (feature :d)
+                    (feature :e)
+                    (resource_limit :cpu 21 {:a 10 :b 4 :e 1})
+                    (selected [:a :c])])
+           solution (configuration model)]
+      (is (has solution :a :c))
+      (is (<= (get solution :cpu) 21)))))
+
+
+(deftest pure-resource-constraints-test-with-exactly-one-solution
+  (testing "a feature model with both logical and resource constraints and exactly one viable solution"
+    (let [ model (feature-model [
+                    (feature :a)
+                    (feature :b)
+                    (feature :c)
+                    (feature :d)
+                    (feature :e)
+                    (requires :a 2 3 [:b :c :d])
+                    (requires :e 1 1 [:b :d])
+                    (excludes :e [:c])
+                    (resource_limit :cpu 11 {:a 10 :b 4 :e 1})
+                    (selected [:a :c])])
+           solution (configuration model)]
+      (is (has solution :a :c :d))
+      (is (not (has solution :b :e)))
+      (is (<= (get solution :cpu) 11)))))
+
+
+(deftest pure-resource-constraints-of-multiple-types
+  (testing "a feature model with multiple types of resource constraints and no logical constraints"
+    (let [ model (feature-model [
+                    (feature :a)
+                    (feature :b)
+                    (feature :c)
+                    (feature :d)
+                    (feature :e)
+                    (resource_limit :cpu 11 {:a 10 :b 4 :e 1})
+                    (resource_limit :memory 8 {:a 6 :b 4 :c 1})
+                    (resource_limit :network 3 {:a 2 :b 4 :e 1})
+                    (selected [:a :c])])
+           solution (configuration model)]
+      (is (has solution :a :c))
+      (is (not (has solution :b :e)))
+      (is (<= (get solution :cpu) 11))
+      (is (<= (get solution :memory) 8))
+      (is (<= (get solution :network) 3)))))
+
+
+(deftest pure-resource-constraints-test-non-viable
+  (testing "a feature model with only resource constraints and no viable solution"
+    (let [ model (feature-model [
+                    (feature :a)
+                    (feature :b)
+                    (feature :c)
+                    (feature :d)
+                    (feature :e)
+                    (resource_limit :cpu 9 {:a 10 :b 4 :e 1})
+                    (selected [:a :c])])
+           solution (configuration model)]
+      (is (not (has solution :a)))
+      (is (not (has solution :b)))
+      (is (not (has solution :c)))
+      (is (not (has solution :d)))
+      (is (not (has solution :e))))))
